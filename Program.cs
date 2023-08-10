3f1599f666f20bab4da2943e776c95b9306c2ee0
Montana Burr <bur18019@byui.edu>
2023-08-10 11:46:54 -0600

    Added RelatedAnimal and Dog classes

--- Program.cs
+++ Program.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 namespace app
 {
-    class Friend
+    abstract class RelatedAnimal
     {
         public string Name;
         public string Id;
+    }
+    class Dog : RelatedAnimal
+    {
+        public string Breed;
+        public Dog(String name, String breed)
+        {
+            this.Name = name;
+            this.Breed = breed;
+        }
+        public override String ToString()
+        {
+            return $"Name: {this.Name}, Breed: {this.Breed}";
+        }
+    }
+    class Friend : RelatedAnimal
+    {
         public string BirthDate;
         public string PhoneNumber;
         public Friend(string name, string BirthDate, string PhoneNumber)
@@ -15,7 +31,7 @@ namespace app
             this.PhoneNumber = PhoneNumber;
             this.Id = Guid.NewGuid().ToString();
         }
-        public String ToString()
+        public override String ToString()
         {
             return $"Name: {this.Name}, Phone Number: {this.PhoneNumber}, Birth Date: {this.BirthDate}";
         }
@@ -28,6 +44,8 @@ namespace app
             friends.Add(new Friend("Linus", "01/01/1970", "707-419-4447"));
             friends.Add(new Friend("Scott", "10/10/1970", "801-224-7513"));
             friends.Add(new Friend("Charlie", "01/01/1999", "801-225-8597"));
+            var dog = new Dog("Happy", "Border Collie");
+            Console.WriteLine(dog.ToString());
             foreach (var friend in friends)
             {
                 Console.WriteLine(friend.ToString());
